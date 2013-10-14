c9979776efb1c16f3606b803f35eba3f92694bb7
Mael Barbin <m.barbin@dental-moving.com>
2013-10-14 14:45:05 +0200

    Initialize the java project in src folder

---
+++ src/hadl/org/alma/hadl/general/IProperty.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.alma.hadl.general;
+
+/**
+ * @author Maël
+ *
+ */
+public interface IProperty {
+
+}
---
+++ src/hadl/org/alma/hadl/link/IBindingLink.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.alma.hadl.link;
+
+/**
+ * @author Maël
+ *
+ */
+public interface IBindingLink {
+
+}
